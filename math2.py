9eb2d034a071c04f67d8001fa7a64746d6a6f4b2
Lumi Cojo <lmcjcr@gmail.com>
2022-02-23 18:34:23 -0500

    add math2

---
+++ math2.py
@@ -0,0 +1,13 @@
+#create 2 variables for the string 6 and 1
+#substract 6-1 
+#save the answer to a new variable 
+#print the str answer and the int answer
+
+numbera_str = "6"
+numberb_str = "1"
+#bellow creating new variable = to the output return value of the int()(function) given the str"argument
+numbera_int = int(numbera_str)
+numberb_int = int(numberb_str)
+answer = numbera_int- numberb_int
+print(answer )
+answer_str = str(answer)
