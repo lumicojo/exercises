40f3d53235bb3c677efb2aaa76532c82be21e647
Lumi Cojo <lmcjcr@gmail.com>
2022-02-24 18:41:17 -0500

    add conversion

---
+++ conversion1.py
@@ -0,0 +1,11 @@
+#make a variable for the int 14
+#make a variable for the "16"
+#add 14 and 16 to make a variable
+#convert the answer to a str 
+
+cone1 = 14
+cone2 = "16"
+cone2_int = int(cone2)
+answer = cone1 + cone2_int
+answer = str(answer)
+print("I love the code", answer)
