709f3bfb3c7fa9d2b695df0357c4541fef2e458a
Lumi Cojo <lmcjcr@gmail.com>
2022-02-24 16:52:44 -0500

    add new problem

---
+++ math3.py
@@ -0,0 +1,15 @@
+# create two variables for the strings 7 and 12
+# multiply 7 and 12 and save the solution to a variable 
+# print out a sentence that says 7 times 12 is 84 which is an int
+#use the veriables
+
+first = "12"
+second = "7"
+first_int = int(first) 
+second_int = int(second)
+answer = first_int * second_int
+answer_type = type(answer)
+print(second, "*", first, "is", answer, "wich is an ",answer_type)
+
+
+
