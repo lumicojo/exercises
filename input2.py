7aea430e5beff28e579ae1c52d95b19b89a0c356
Lumi Cojo <lmcjcr@gmail.com>
2022-02-27 09:37:52 -0500

    add more practice

---
+++ input2.py
@@ -0,0 +1,6 @@
+# prompt the user for their name 
+# prompt the user for their age
+# return the sentence using their name and age that says Name is Age years old
+name = input("what is your name?")
+age = input("how old are you?")
+print(name, "is", age, "years", "old")
