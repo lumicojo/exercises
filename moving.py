677a9848e7a786eb7c1f57f059b84568a17ca51e
Lumi Cojo <lmcjcr@gmail.com>
2022-03-01 17:35:06 -0500

    add more exercice imput

---
+++ moving.py
@@ -0,0 +1,11 @@
+#lumi is from romania
+#lumi moved to new jersey
+#lumi is going to be a programer
+#lumi is going to move to sandiego
+
+name = input("what is your name?")
+country = input("where are you from?")
+come = input("where did you moved?")
+job = input("are you going to job?")
+move = input("where are you landing")
+print(name, "is from", country, "and she setall in", come,  "si is going to be a",job, "and leave in", move, )
