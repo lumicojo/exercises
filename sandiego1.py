d409b18f316077d0ea1609e279d1df8013d625ef
Lumi Cojo <lmcjcr@gmail.com>
2022-03-03 18:31:05 -0500

    add one more problem

---
+++ sandiego1.py
@@ -0,0 +1,7 @@
+#carmen  find appartament in sandiego
+#carmen is going to fly again in california
+
+name  = input("what is your name?")
+place = input("what did you find?")
+state = input("were are you going to fly?")
+print(name, " find", place, "in sandiego she is going to fly in", state)
