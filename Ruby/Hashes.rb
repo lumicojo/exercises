664bd4015a0cbce98a86b407764a3896d388791d
Lumi Cojo <lmcjcr@gmail.com>
2022-05-10 20:40:24 -0400

    ruby exercises

---
+++ Ruby/Hashes.rb
@@ -0,0 +1,26 @@
+my_hash = { "name" => "Eric",
+  "age" => 26,
+  "hungry?" => true
+}
+
+puts my_hash["name"]
+puts my_hash["age"]
+puts my_hash["hungry?"]
+
+hash = {
+  key1 => value1,
+  key2 => value2,
+  key3 => value3
+}
+
+pets = Hash.new
+pets["pet"] = 'dog'
+
+#Access the key-value pair you added to pets, just like step 2 in the example above.
+#Use puts to print that value 
+pets = {
+  "pet" => "dog"
+}
+puts pets ["pet"]
+# will print "dog"
+
