0f4718a3ff24f583c087eebca48acd8f9316c9f4
Lumi Cojo <lmcjcr@gmail.com>
2022-05-16 18:44:09 -0400

    practice loops

---
+++ Ruby/loop.rb
@@ -0,0 +1,13 @@
+#Use a while loop to print out the numbers 1 through 50, inclusive.
+#counter = 1
+#while counter <= 50 
+  #print  counter
+  #counter += 1  
+#end  
+# rewrite your while loop using until. You still want to print out the numbers 1 through 50, inclusive.
+counter = 0
+while counter >= 50
+    print counter
+    counter -= 1
+end   
+
