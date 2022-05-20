66ecf84f4988e0e9324235b44731cb3df3116006
Lumi Cojo <lmcjcr@gmail.com>
2022-05-20 11:36:12 -0400

    iterate ovrer arrays and hashes

--- Ruby/Hashes.rb
+++ Ruby/Hashes.rb
@@ -7,20 +7,13 @@ puts my_hash["name"]
 puts my_hash["age"]
 puts my_hash["hungry?"]
 
+
+#hash is a collection of key-value pairs. Hash syntax looks like this:
 hash = {
   key1 => value1,
   key2 => value2,
   key3 => value3
 }
-
-pets = Hash.new
-pets["pet"] = 'dog'
-
-#Access the key-value pair you added to pets, just like step 2 in the example above.
-#Use puts to print that value 
-pets = {
-  "pet" => "dog"
-}
-puts pets ["pet"]
-# will print "dog"
+#Values are assigned to keys using =>.
+# You can use any Ruby object for a key or value.
 
---
+++ Ruby/hashesiterat1.rb
@@ -0,0 +1,5 @@
+friends = ["Kim","Ken","Lexy"]
+family = {"Lumi" => "mom","Carmen" => "doghter", "Roxy" => "sister",
+    "Kevin" => "son", "Sushi" => "dog"}
+friends.each{ |x| puts "#{x}" }
+family.each{ |x,y| puts "#{x} : #{y} "}    
