ea29455ac7c816854b79b69d23f2e68f1bd65f7e
+ <+>
2022-09-08 16:24:46 -0400

    practice

--- Ruby/Ruby-codeacademy/variable.rb
+++
@@ -1,4 +0,0 @@
-#Create a variable named orange & give it a value of 300.
-#Create another variable named apple & give it a value of 120.
-#Multiply the value of orange & apple, then assign the result to a new variable total.
-
